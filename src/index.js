6be1f154b9ccb1d0ea88984ee06e30bad35c86e3
keypanic <kaigauscom@gmail.com>
2020-10-01 04:45:00 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || !array.length)
+    return 0;
+  let minNumber = array[0];
+  for(i = 1; i < array.length; i++) {
+    if(minNumber > array[i])
+      minNumber = array[i];
+  }
+  return minNumber;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || !array.length)
+    return 0;
+  let maxNumber = array[0];
+  for(i = 1; i < array.length; i++) {
+    if(maxNumber < array[i])
+      maxNumber = array[i];
+  }
+  return maxNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || !array.length)
+    return 0;
+  avgNumber = 0;
+  for(i = 0; i < array.length; i++) { 
+    avgNumber += array[i];
+  }
+  return avgNumber/array.length;
 }
